85737dbd0f70417d04973c7515c1492184366694
Pankratov <pnrf@yandex.ru>
2022-02-13 23:29:06 +0300

    bugfix: semicolon in .places__heart-button

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,7 @@
-// Выборка DOM элементов
+//открытие и закрытие popup
 const popupElement = document.querySelector('.popup');
 const popupEditButtonElement = document.querySelector('.profile__edit-button');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
-
-const likeButtonElement = document.querySelector('.places__heart-button'); //для черного сердечка на 5-ый спринт
 
 const openPopup = function() {
   popupElement.classList.add('popup_opened');
@@ -13,11 +11,30 @@ const closePopup = function() {
   popupElement.classList.remove('popup_opened');
 };
 
-const toggleLikeButton = function() {
-  likeButtonElement.classList.toggle('places__heart-button_active'); //для черного сердечка на 5-ый спринт
-};
-
-// Обработчики событий
 popupEditButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
-likeButtonElement.addEventListener('click', toggleLikeButton); //для черного сердечка на 5-ый спринт
+
+//попап содержит информацию ('имя' и 'о себе') из профиля
+let profileNameElement = document.querySelector('.profile__title');
+let profileAboutElement = document.querySelector('.profile__subtitle');
+let popupNameElement = document.querySelector('.popup__input-text_type_name');
+let popupAboutElement = document.querySelector('.popup__input-text_type_about');
+
+popupNameElement.setAttribute('placeholder', profileNameElement);
+popupAboutElement.setAttribute('placeholder', profileAboutElement);
+
+
+//попап редактирует информацию профиля на странице
+
+
+
+//ставим лайк на карточке (для черного сердечка на 5-ый спринт)
+//пока работает только на 1-ой карточке
+
+const likeButtonElement = document.querySelector('.places__heart-button');
+
+const toggleLikeButton = function() {
+  likeButtonElement.classList.toggle('places__heart-button_active');
+};
+
+likeButtonElement.addEventListener('click', toggleLikeButton);
