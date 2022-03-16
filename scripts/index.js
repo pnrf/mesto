1b8b3f33b994424a8c231cc8a83c171be3773f7a
Pankratov <pnrf@yandex.ru>
2022-03-16 16:04:55 +0300

    refactor: rewrite almost all js code

--- scripts/index.js
+++ scripts/index.js
@@ -1,190 +1,132 @@
-/*
-// (1) Данные профиля по умолчанию (Жак-Ив Кусто, исследователь океана), отображение на странице
+// (1) Constants and variables
 
-const profileInitialData = [
-  'Жак-Ив Кусто',
-  'Исследователь океана'
-]; // массив со значениями полей профиля по умолчанию
+// (1.1) кнопки на странице
+const editBtnElement = document.querySelector('.profile__edit-button'); // ссылка на кнопку "редактировать профиль"
+const addBtnElement = document.querySelector('.profile__add-button'); // ссылка на кнопку "добавить новую карточку"
 
-let profileNameElement = document.querySelector('.profile__title'); // ссылка на имя
-let profileAboutElement = document.querySelector('.profile__subtitle'); // ссылка на описание
+// (1.2) POPUP 1: profile editing form
+const popupProfileElement = document.querySelector('.popup_type_profile'); // ссылка на popup для редактирования профиля
 
-profileNameElement.textContent = profileInitialData[0]; // подставляем имя из массива
-profileAboutElement.textContent = profileInitialData[1]; // подставляем описание из массива
-*/
+const popupCloseBtnElement = popupProfileElement.querySelector('.popup__close-button'); // ссылка на крестик в popup
+const popupSaveBtnElement = popupProfileElement.querySelector('.popup__save-button'); // ссылка на кнопку "сохранить" в попапе
+const popupProfileForm = popupProfileElement.querySelector('.popup__input-list'); // ссылка на форму
+const popupNameElement = popupProfileElement.querySelector('.popup__input-text_type_name'); // ссылка на поле "имя" в попапе
+const popupAboutElement = popupProfileElement.querySelector('.popup__input-text_type_about'); // ссылка на поле "описание" в попапе
 
-// (2) POPUP-IMG -- для просмотра картинок
-const popupImgElement = document.querySelector('.popup-img'); // ссылка на popup-img
-const popupImgCloseBtnElement = popupImgElement.querySelector('.popup__closebtn'); // крестик в popup-img
+const profileElement = document.querySelector('.profile'); // ссылка на профиль
+const profileNameElement = profileElement.querySelector('.profile__title'); // ссылка на имя (Жак-Ив Кусто) на странице
+const profileAboutElement = profileElement.querySelector('.profile__subtitle'); // ссылка на описание (Исследователь океана) на странице
 
-// (2.1) закрытие попапа
-const closePopupImg = function() {
-  popupImgElement.classList.remove('popup-img_opened');
+// (1.3) POPUP 2: card adding form
+const popupCardsElement = document.querySelector('.popup_type_cards'); // ссылка на popup для добавления новой карточки
+
+const popupCardsCloseBtnElement = popupCardsElement.querySelector('.popup__close-button'); // ссылка на крестик в popup
+const popupCardsSaveBtnElement = popupCardsElement.querySelector('.popup__save-button'); // ссылка на кнопку "создать" в попапе
+const popupCardsForm = popupCardsElement.querySelector('.popup__input-list'); // ссылка на форму
+const popupPlaceElement = popupCardsElement.querySelector('.popup__input-text_type_name'); // ссылка на поле "название места" в попапе
+const popupLinkElement = popupCardsElement.querySelector('.popup__input-text_type_about'); // ссылка на поле "ссылка на картинку" в попапе
+
+// (1.4) POPUP 3: image preview
+const popupImageElement = document.querySelector('.popup_type_image'); // ссылка на popup для просмотра картики
+const popupImgCloseBtnElement = popupImageElement.querySelector('.popup__close-button'); // ссылка на крестик в popup
+
+// (1.5) TEMPLATE for cards
+const itemTemplate = document.querySelector('#template'); // ссылка на темплейт
+const listElement = document.querySelector('.cards__list'); // ссылка на родителя (куда вставить темплейт)
+
+
+
+// (2) Functions
+
+// (2.1) функция: создать карточку из темлейта:
+const createCard = function(item) {
+  // клонировал темлейт из html в DOM:
+  const itemElement = itemTemplate.content.cloneNode(true);
+  const cardImgElm = itemElement.querySelector('.card__image');
+  const popupImgElm = popupImageElement.querySelector('.popup__image');
+
+  // наполнил темплейт содержимым:
+  itemElement.querySelector('.card__title').textContent = item.name;
+  cardImgElm.src = item.link;
+  cardImgElm.alt = `${item.name}. Фотография`;
+  // слушатель на картинке:
+  itemElement.querySelector('.card__image').addEventListener('click', function() {
+    openPopup(popupImageElement);
+    popupImageElement.querySelector('.popup__figcaption').textContent = item.name;
+    popupImgElm.src = item.link;
+    popupImgElm.alt = `${item.name}. Фотография`;
+  });
+  // слушатель для лайка/дизлайка:
+  itemElement.querySelector('.card__like-button').addEventListener('click', function (event) {
+    event.target.classList.toggle('card__like-button_active');
+  });
+  // слушатель для удаления карточки:
+  itemElement.querySelector('.card__del-button').addEventListener('click', function (event) {
+    event.target.closest('.card').remove();
+  });
+
+  return itemElement;
 };
 
-popupImgCloseBtnElement.addEventListener('click', closePopupImg); //обработчик события
-
-
-// (3) при загрузке на странице должно быть 6 карточек, которые добавит javascript
-
-// (3.1) функция отрисовки карточек на странице:
-const itemTemplateContent = document.querySelector('.item-template'); // ссылка на темплейт
-const listElement = document.querySelector('.places__list'); // ссылка на родителя
-
-function renderCards(item) {
-    // клонировал темлейт из html в DOM:
-    const itemElement = itemTemplateContent.content.cloneNode(true);
-    // наполнил темплейт содержимым:
-    itemElement.querySelector('.places__card-title').textContent = item.name;
-    itemElement.querySelector('.places__card-image').src = item.link;
-    itemElement.querySelector('.places__card-image').alt = `${item.name}. Фотография`;
-    // слушатель на картинке:
-    itemElement.querySelector('.places__card-image').addEventListener('click', function(event) {
-      popupImgElement.classList.add('popup-img_opened');
-      popupImgElement.querySelector('.popup-img__text').textContent = item.name;
-      popupImgElement.querySelector('.popup-img__image').src = item.link;
-      popupImgElement.querySelector('.popup-img__image').alt = `${item.name}. Фотография`;
-    });
-    // функция лайк/дизлайк:
-    itemElement.querySelector('.places__heart-button').addEventListener('click', function (event) {
-      event.target.classList.toggle('places__heart-button_active');
-    });
-    // функция удаления карточки (кнопка корзина):
-    const delBtn = itemElement.querySelector('.places__basket'); // ссылка на значок корзины в попапе
-
-    delBtn.addEventListener('click', function (event) {
-      const delItem = delBtn.closest('.places__card');
-      delItem.remove();
-    });
-    // отрисовал карточку вначале списка:
-    listElement.prepend(itemElement);
-};
-
-// (3.2) исходный массив объектов для отрисовки карточек на странице
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-// (3.3) развернул исходный массив, перебрал его и отрисовывал карточки на странице:
-const arrayReversed = initialCards.reverse();
-arrayReversed.forEach(function(item) {
-  renderCards(item);
+// (2.2) функция перебрать массив, отрисовать карточки по порядку
+initialCards.forEach(function(item) {
+  listElement.appendChild(createCard(item)); // отрисовал карточки по порядку
 });
 
-
-// (4) POPUP - для добавления карточек и редактирования профиля
-
-// (4.1) закрыть попап нажатием на крестик
-const popupElement = document.querySelector('.popup'); // ссылка на popup
-const popupCloseBtnElement = popupElement.querySelector('.popup__closebtn'); // ссылка на крестик в popup
-
-const closePopup = function() {
-  popupElement.classList.remove('popup_opened');
+// (2.3) функция отрисовать новую карточку в начале списка:
+const renderCard = function(name, link) {
+  const item = {
+      name,
+      link
+    };
+  listElement.prepend(createCard(item)); // отрисовал карточку в начале списка
+  popupPlaceElement.value = ''; // чтобы при добавлении новой карточки, прежние значения обнулялись
+  popupLinkElement.value = ''; // иначе, они остаются в попапе и перебивают placeholder
 };
 
-popupCloseBtnElement.addEventListener('click', closePopup); //обработчик события
+// (2.4) функция: открыть попап
+const openPopup = function(item) {
+  item.classList.add('popup_opened');
+};
+
+// (2.5) функция: закрыть попап
+const closePopup = function(item) {
+  item.classList.remove('popup_opened');
+};
 
 
-// (4.2) редактировать профиль (Жак-Ив Кусто, исследователь океана)
 
-// (4.2.1) открыть попап при клике на кнопку "редактировать", подставить в попап значения
-const editBtnElement = document.querySelector('.profile__edit-button'); // ссылка на кнопку "редактировать"
+// (3) Event handlers
 
-const popupTitle = popupElement.querySelector('.popup__title'); // ссылка на заголовок "Редактировать профиль" в попапе
-const popupSaveBtnElement = popupElement.querySelector('.popup__save-button'); // ссылка на кнопку "сохранить" в попапе
-const popupNameElement = popupElement.querySelector('.popup__input-text_type_name'); // ссылка на поле "имя" в попапе
-const popupAboutElement = popupElement.querySelector('.popup__input-text_type_about'); // ссылка на поле "описание" в попапе
+// (3.1) редактировать профиль (Жак-Ив Кусто, исследователь океана)
 
-const openPopupEditor = function() {
-  popupTitle.textContent = 'Редактировать профиль';
-  popupSaveBtnElement.textContent = 'Сохранить';
-  popupSaveBtnElement.setAttribute('type', 'button');
+editBtnElement.addEventListener('click', function(){
+  openPopup(popupProfileElement);
   popupNameElement.value = profileNameElement.textContent;
   popupAboutElement.value = profileAboutElement.textContent;
-  popupElement.classList.add('popup_opened');
-};
+}, false); // открыть попап при клике на кнопке "редактировать"
 
-editBtnElement.addEventListener('click', openPopupEditor); // обработчик события на кнопке "редактировать"
+popupCloseBtnElement.addEventListener('click', function(){closePopup(popupProfileElement)}, false); //закрыть попап при клике на крестик
 
-// (4.2.2) изменить данные профиля на странице (кнопка сохранить)
-const editPopup = function() {
+popupProfileForm.addEventListener('submit', function(evt){
+  evt.preventDefault();
   profileNameElement.textContent = popupNameElement.value;
   profileAboutElement.textContent = popupAboutElement.value;
-  popupElement.classList.remove('popup_opened'); //при нажатии на кнопку "сохранить" попап закрывается
-};
+  closePopup(popupProfileElement);
+}, false); // изменить данные профиля на странице при клике на кнопку "сохранить"
 
-// обработчик события перенесен в п.4.4., т.к. клик по кнопке влечет разные последствия
+// (3.2) добавить новую карточку
 
+addBtnElement.addEventListener('click', function(){openPopup(popupCardsElement);}, false); // открыть попап при клике на кнопке "добавить"
 
-// (4.3) Добавление новой карточки
+popupCardsCloseBtnElement.addEventListener('click', function(){closePopup(popupCardsElement)}, false); //закрыть попап при клике на крестик
 
-// (4.3.1) открыть попап при клике на кнопку "добавить", подставить в попап значения
-const addBtnElement = document.querySelector('.profile__add-button'); // ссылка на кнопку "добавить"
+popupCardsForm.addEventListener('submit', function(evt){
+  evt.preventDefault();
+  renderCard(popupPlaceElement.value, popupLinkElement.value);
+  closePopup(popupCardsElement);
+}); // отрисовать новую карточку на странице в начале списка
 
-const openPopupAddCard = function() {
-  popupTitle.textContent = 'Новое место';
-  popupSaveBtnElement.textContent = 'Создать';
-  popupNameElement.setAttribute('placeholder', 'Название');
-  popupAboutElement.setAttribute('placeholder', 'Ссылка на картинку');
-  popupSaveBtnElement.setAttribute('type', 'submit');
-  popupNameElement.value = ''; // чтобы value не перебивало placeholder
-  popupAboutElement.value = ''; // чтобы value не перебивало placeholder
-  popupElement.classList.add('popup_opened');
-};
+// (3.3) закрыть попап с картинкой
 
-addBtnElement.addEventListener('click', openPopupAddCard); //обработчик события на кнопке "добавить"
-
-// (4.3.2) добавить новую карточку в массив и на страницу (кнопка попапа "создать")
-const addCard = function() {
-  let item = {
-    name: popupNameElement.value,
-    link: popupAboutElement.value
-  };
-
-  initialCards.unshift(item);
-
-renderCards(item); // функция отрисовки карточки на странице
-
-popupElement.classList.remove('popup_opened'); //при нажатии на кнопку "создать" попап закрывается
-};
-
-
-// (4.4) обработчик события при добавлении новой карточки и при редактировании профиля
-// эта функция разграничивает последствия, т.к. попап один и тот же для редактирования профиля и для добавления карточек
-function setEventHandler() {
-  if (popupTitle.textContent === 'Новое место') {
-    addCard(); //функция для добавления новой карточки
-  } else if (popupTitle.textContent === 'Редактировать профиль') {
-    editPopup(); //функция для редактирования данных профиля
-  } else {
-    popupElement.classList.remove('popup_opened');
-  }; // если что-то не сработает, попап просто закрывается без внесения каких-либо изменений на страницу
-};
-
-popupSaveBtnElement.addEventListener('click', setEventHandler); // обработчик события при клике на кнопку попапа
-
-
-
+popupImgCloseBtnElement.addEventListener('click', function(){closePopup(popupImageElement)}, false); //закрыть попап при клике на крестик
