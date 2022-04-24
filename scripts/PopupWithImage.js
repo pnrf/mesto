e96701185bbf003d616c41b14a849a2b914457d5
Pankratov <pnrf@yandex.ru>
2022-04-24 21:11:36 +0300

    refactor: add js-files with new classes (prototypes)

---
+++ scripts/PopupWithImage.js
@@ -0,0 +1,14 @@
+/** Создайте класс PopupWithImage, который наследует от Popup.
+ * Этот класс должен перезаписывать родительский метод open.
+ * В методе open класса PopupWithImage нужно вставлять в попап картинку с src изображения и подписью к картинке.
+*/
+import Popup from './Popup.js'
+
+export default class PopupWithImage extends Popup {
+  constructor() {}
+
+  /** openPopupWithImage - публичный метод: вставить в попап картинку (src), alt и подпись к картинке */
+  openPopupWithImage() {
+
+  }
+}
