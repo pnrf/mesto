9b62ca1cfc812378e1d8f683e00a786de1919e60
Pankratov <pnrf@yandex.ru>
2022-05-27 20:01:57 +0300

    docs: update Readme & add comments to FormValidator.js

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -98,3 +98,37 @@ export default class FormValidator {
   };
 }
 
+
+/** ---------- Проработать универсальную валидацию ----------*/
+
+/*
+Если будет интересно, можно универсально создать экземпляры валидаторов всех форм,
+поместив их все в один объект, а потом брать из него валидатор по атрибуту name,
+который задан для формы. Это очень универсально и для любого кол-ва форм подходит.
+
+const formValidators = {}
+
+// Включение валидации
+const enableValidation = (config) => {
+  const formList = Array.from(document.querySelectorAll(config.formSelector))
+  formList.forEach((formElement) => {
+    const validator = new FormValidator(formElement, config)
+// получаем данные из атрибута `name` у формы
+    const formName = formElement.getAttribute('name')
+
+   // вот тут в объект записываем под именем формы
+    formValidators[formName] = validator;
+   validator.enableValidation();
+  });
+};
+
+enableValidation(config);
+
+----
+И теперь можно использовать валидаторы для деактивации кнопки и тд
+
+formValidators[ profileForm.getAttribute('name') ].resetValidation()
+
+// или можно использовать строку (ведь Вы знаете, какой атрибут `name` у каждой формы)
+formValidators['profile-form'].resetValidation()
+*/
